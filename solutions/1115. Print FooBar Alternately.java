fc126d1dbf98c94887bd226aa36012f90043622e
Devina Dhuri <ddhuri1@binghamton.edu>
2020-06-23 07:57:05 -0400

    17 ms

--- solutions/1115. Print FooBar Alternately.java
+++ solutions/1115. Print FooBar Alternately.java
@@ -1,25 +1,44 @@
-class FooBar {
+class FooBar
+{
     private int n;
-
-    public FooBar(int n) {
+    private final static ReentrantLock re = new ReentrantLock();
+    private final static Condition cond= re.newCondition();
+    private boolean foo;
+    
+    public FooBar(int n) 
+    {
         this.n = n;
+        this.foo = true;
     }
 
-    public void foo(Runnable printFoo) throws InterruptedException {
-        
-        for (int i = 0; i < n; i++) {
-            
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
+    // printFoo.run() outputs "foo". Do not change or remove this line.
+    public void foo(Runnable printFoo) throws InterruptedException 
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            re.lock();
+            while (foo != true) 
+                cond.await();
         	printFoo.run();
+            foo = false;
+            cond.signal();
+            re.unlock();
         }
     }
 
-    public void bar(Runnable printBar) throws InterruptedException {
+    // printBar.run() outputs "bar". Do not change or remove this line.
+    public void bar(Runnable printBar) throws InterruptedException 
+    {
         
-        for (int i = 0; i < n; i++) {
-            
-            // printBar.run() outputs "bar". Do not change or remove this line.
+        for (int i = 0; i < n; i++) 
+        {
+            re.lock();
+            while (foo == true) 
+                cond.await();
         	printBar.run();
+            foo = true;
+            cond.signal();
+            re.unlock();
         }
     }
 }
